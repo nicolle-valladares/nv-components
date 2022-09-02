4d4f1d8adc00ef12e28757626883650dcbed69a7
abigailalvarado <aalvarado@redabierta.hn>
2022-09-02 14:34:39 -0600

    SAL-8 Create-checkbox-component

---
+++ src/components/Checkbox/Checkbox.stories.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+import { Checkbox } from './Checkbox'
+
+export default {
+  title: 'Components/Checkbox',
+  component: Checkbox
+} as ComponentMeta<typeof Checkbox>
+
+const Template: ComponentStory<typeof Checkbox> = args => <Checkbox {...args} />
+
+export const RACheckbox = Template.bind({})
+RACheckbox.args = {
+  children: 'Hello world!',
+}
---
+++ src/components/Checkbox/Checkbox.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Checkbox as AntdCheckbox } from 'antd'
+
+export interface CheckboxProps {
+  /** Children node for the checkbox */
+  children?: React.ReactNode
+  /** CSS styles */
+  style?: React.CSSProperties
+  /** If disable checkbox */
+  disabled?: boolean
+  /** Specifies whether the checkbox is selected*/
+  checked?: boolean
+  /** Class for the checkbox element */
+  className?: string
+}
+
+export const Checkbox = ({
+  children,
+  disabled,
+  checked,
+  ...props
+}: CheckboxProps) => (
+  <StyledCheckbox checked={checked} disabled={disabled} {...props}>
+    {children}
+  </StyledCheckbox>
+)
+
+const StyledCheckbox = styled(AntdCheckbox)`
+
+  .ant-checkbox-inner {
+    width: 18px;
+    height: 18px;
+    border: 2px solid ${props => props.theme.colors.primaryColor};
+  }
+
+  .ant-checkbox-checked .ant-checkbox-inner{
+    border-color: ${props => props.theme.colors.primaryColor};
+    background-color: ${props => props.theme.colors.primaryColor};
+  }
+
+`
